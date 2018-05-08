ac9756c1bd2a0e87c5b16ea4de2867f0b863dc0d
gtDataLab <gregg.tyson@gmail.com>
2018-05-08 12:53:47 -0400

    Added projects page

--- server.js
+++ server.js
@@ -58,6 +58,12 @@ app.get('/bad', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects Page'
+	});
+});
+
 // Start Server
 app.listen(port, () => {
 	console.log(`Server is up on ${port}`)
